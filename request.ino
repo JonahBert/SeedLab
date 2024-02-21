724e8c253322bf15e7c5012172a38580e59dd9a1
joeleybean <103665401+joeleybean@users.noreply.github.com>
2024-02-21 14:01:47 -0700

    Add files via upload

---
+++ request.ino
@@ -0,0 +1,8 @@
+void request() {
+  // According to the Wire source code, we must call write() within the requesting ISR
+  // and nowhere else. Otherwise, the timing does not work out. See line 238:
+  // https://github.com/arduino/ArduinoCore-avr/blob/master/libraries/Wire/src/Wire.cpp
+  Wire.write(reply);
+  Serial.println("Request Recieved");
+  reply = 0;
+}
